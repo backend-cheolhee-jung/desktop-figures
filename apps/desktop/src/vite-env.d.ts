c92a7b48349d4dfb66844852abc10307912c77da
cheolhee <cheolhee.jung@a-bly.com>
2026-05-31 14:31:53 +0900

    chore: replace Vertex env types with Meshy API key

--- apps/desktop/src/vite-env.d.ts
+++ apps/desktop/src/vite-env.d.ts
@@ -3,9 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_AUTH_SERVER_URL: string;
   readonly VITE_SOCKET_SERVER_URL: string;
-  readonly VITE_VERTEX_AI_TOKEN: string;
-  readonly VITE_VERTEX_PROJECT_ID: string;
-  readonly VITE_VERTEX_LOCATION: string;
+  readonly VITE_MESHY_API_KEY: string;
 }
 
 interface ImportMeta {
